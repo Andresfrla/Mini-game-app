0a8d2cb3dc33e53f18471953687fed0212d36e0f
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-04-12 20:19:15 -0600

    create another file for the colors on different platforms

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, Platform } from "react-native";
 function Title ({children}) {
     return <Text style={styles.title}>{children}</Text>
 };
@@ -9,7 +9,8 @@ const styles = StyleSheet.create({
         fontSize: 24,
         color: 'white',
         textAlign: "center",
-        borderWidth: 2,
+        // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        // borderWidth: Platform.select({ ios: 0, android: 2 }),
         padding: 12,
         borderColor: 'white',
         marginTop: 24,
